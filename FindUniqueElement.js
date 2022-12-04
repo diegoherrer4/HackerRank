ddc356744c7a72951ab423fb3cb129f061ccd868
Diego Herrera <dagomessis@gmail.com>
2022-12-04 18:37:52 -0500

    2

--- FindUniqueElement.js
+++ FindUniqueElement.js
@@ -2,7 +2,7 @@
 
 // Given an array of integers, where all elements but one occur twice, find the unique element.
 
-function lonelyinteger(a){
+function lonelyinteger(a) {
     return a.filter(e => a.indexOf(e) === a.lastIndexOf(e))
 
 }
