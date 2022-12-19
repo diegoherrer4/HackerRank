335531b6f910abf10d82e940ff51f5171f7a66fd
Diego Herrera <dagomessis@gmail.com>
2022-12-18 23:18:24 -0500

    1

--- ContainsDuplicate.js
+++ ContainsDuplicate.js
@@ -1,6 +1,6 @@
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
-var containsDuplicate = function(nums) {
+var containsDuplicate = function(nums){
     const unique = [...new Set(nums)]
     return nums === unique ? false : true
 };
